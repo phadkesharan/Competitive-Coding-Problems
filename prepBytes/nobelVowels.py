a98e146ab183c35a8f6a10d7047b69e8b6ce522f
Sharan Phadke <phadkesharan@gmail.com>
2021-11-11 19:48:06 +0530

    gitignore update

--- prepBytes/nobelVowels.py
+++ prepBytes/nobelVowels.py
@@ -1,30 +1,33 @@
-t = int(input())
-
 vowels = ['a', 'e', 'i', 'o', 'u']
 
-while t:
-  
-  s = str(input())
-  s = s.lower()
-  flag = True
-  
-  if len(s) == 1:
-    print("YES")
-    continue
-  
-  for i in range(len(s)-1):
-    if s[i] not in vowels and s[i].isalpha():
-      if s[i+1] not in vowels:
-        flag =False
-        break
-      
-  if flag:
-    print("YES")
-  else:
-    print("NO")
+def isNobel(s):
+
+    if len(s) == 1:
+        if s[0] in vowels or s[0] == 'n':
+            return True
     
-  t -= 1
-      
-      
-  
-      
+    if s[len(s)-1] not in vowels and s[len(s)-1] != 'n':
+        return False
+
+    for i in range(len(s)-1):
+        if s[i] not in vowels:
+            if s[i+1] not in vowels:
+                return False
+
+    return True
+
+
+t = int(input())
+
+while t:
+
+    s = str(input())
+
+    ans = isNobel(s)
+
+    if ans:
+        print("YES")
+    else:
+        print("NO")
+
+    t -= 1
