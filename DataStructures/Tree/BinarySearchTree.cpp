ebea9e94c143c9c036020dad1e4f225d6e6bd408
Sharan Phadke <phadkesharan@gmail.com>
2022-01-08 19:30:49 +0530

    adds BST

---
+++ DataStructures/Tree/BinarySearchTree.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left, *right;
+
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inOrder(Node *root) {
+    if(root != NULL) {
+        inOrder(root->left);
+        cout <<root->data <<" ";
+        inOrder(root->right);
+    }
+}
+
+bool BSTSearch(Node *root, int x) {
+    if(root == NULL)
+        return false;
+
+    if(x > root->data)
+        return BSTSearch(root->right, x);
+
+    else if(x < root->data)
+        return BSTSearch(root->left, x);
+
+    return true;
+}
+
+int getMax(Node* root) {
+
+    if(root == NULL)
+        return -1;
+    Node* temp = NULL;
+    while (root != NULL) {
+        temp = root;
+        root = root->right;
+    }
+
+    return temp->data;
+}
+
+int getMin(Node* root) {
+
+    if(root == NULL)
+        return -1;
+    Node* temp = NULL;
+    while (root != NULL) {
+        temp = root;
+        root = root->left;
+    }
+
+    return temp->data;
+}
+
+int main() {
+    Node *root;
+
+    root = new Node(7);
+    root->left = new Node(4);
+    root->right = new Node(9);
+    root->left->left = new Node(2);
+    root->left->right = new Node(5);
+
+    cout <<"max : " <<getMax(root) <<endl;
+    cout <<"min : " <<getMin(root) <<endl;
+}
+
+
+
